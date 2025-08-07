b54275c6d528e19884b96663782d06be223fcc2e
alucard <LeroyKokoti3@gmail.com>
2025-08-07 21:52:23 +0000

    Intergrated models

--- lib/Screens/device_screen.dart
+++ lib/Screens/device_screen.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c773b7c2e40dc31ae36fddbbde09b68bfd011d5d962df624808b4f6682fce0e6
-size 8049
+oid sha256:41c93d8e0fab19ad53e12d05f59910915d87c792f92821fcae4ec359b092d18e
+size 8405
