98c3921caca2f7d722fb187422b078bdf52c47bb
alucard <LeroyKokoti3@gmail.com>
2025-08-08 12:11:33 +0000

    Added ai_data_service,ai_service_provider,local_model_service,model_runners,model_runner_manager,normalization_service,homescreen update,dahsboard update,device screen update, report updated, model device screen integration,model widget integration,model firebase pipelining

--- lib/Screens/device_screen.dart
+++ lib/Screens/device_screen.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:41c93d8e0fab19ad53e12d05f59910915d87c792f92821fcae4ec359b092d18e
-size 8405
+oid sha256:4351916e03be371aaf08093d678576dbd4f1f78d491929fa4d635c6d183d9044
+size 8601
